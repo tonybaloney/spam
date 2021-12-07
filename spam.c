f797ce075303c1d8252dd5b1c94b972670231873
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-12-07 18:45:37 +1100

    Update spam.c

--- spam.c
+++ spam.c
@@ -8,40 +8,6 @@ static PyObject* spam_frame(PyThreadState* ts, PyFrameObject* f, int throwflag) 
     return _PyEval_EvalFrameDefault(ts, f, throwflag);
 }
 
-static PyObject* spam_system(PyObject* self, PyObject* args) {
-    const char* command;
-    int status;
-
-    if (!PyArg_ParseTuple(args, "s", &command)) {
-        return NULL;
-    }
-    status = system(command);
-    if (!WIFEXITED(status)) {
-        return Py_BuildValue("i", -1);
-    }
-    return Py_BuildValue("i", WEXITSTATUS(status));
-}
-
-static PyObject* spam_check_system(PyObject* self, PyObject* args) {
-    const char* command;
-    int status;
-    int exit_status;
-
-    if (!PyArg_ParseTuple(args, "s", &command)) {
-        return NULL;
-    }
-    status = system(command);
-    if (!WIFEXITED(status)) {
-        return PyErr_Format(SpamError, "Command could not be executed");
-    }
-    exit_status = WEXITSTATUS(status);
-    if (exit_status != 0) {
-        return PyErr_Format(
-            SpamError, "Command returned non-zero exit status %d", exit_status);
-    }
-    Py_RETURN_NONE;
-}
-
 static PyInterpreterState* inter() {
     return PyInterpreterState_Main();
 }
@@ -76,7 +42,6 @@ static PyMethodDef SpamMethods[] = {
     {NULL, NULL, 0, NULL},  // sentinel
 };
 
-#if PY_MAJOR_VERSION >= 3
 static PyModuleDef spammodule = {
     PyModuleDef_HEAD_INIT,
     "spam",
@@ -92,16 +57,7 @@ PyMODINIT_FUNC PyInit_spam() {
     if (module == NULL) {
         return NULL;
     }
+    spam_enable();
     return module;
 }
-#else
-PyMODINIT_FUNC initspam() {
-    PyObject* module;
 
-    module = Py_InitModule3(
-        "spam", SpamMethods, "An example Python C extension module.");
-    if (module == NULL) {
-        return;
-    }
-}
-#endif
